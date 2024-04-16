4fbe43f568aa8c60e62b60c1a3482cef93c2fdf5
emilyallegretti <emmy.allegretti@gmail.com>
2024-04-16 12:23:46 -0400

    added missing comments

--- Events/MouseEnterSlides.py
+++ Events/MouseEnterSlides.py
@@ -1,5 +1,5 @@
 from Events.Event import Event
-
+# This Event class represents the action in which a user's mouse cursor enters the slide deck shown on the Readings page.
 
 class MouseEnterSlides(Event):
     def __init__(self, metadata, slideId):
--- Events/MouseLeaveSlides.py
+++ Events/MouseLeaveSlides.py
@@ -1,6 +1,6 @@
 from Events.Event import Event
 
-
+# This Event class describes the action in which a user's mouse cursor leaves the slide deck on the Readings page.
 class MouseLeaveSlides(Event):
     def __init__(self, metadata, slideId):
         super().__init__(metadata, self.__class__.__name__)
